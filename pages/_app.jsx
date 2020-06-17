c535e8dbb01ee4fe97c691aa9ccce5d4c09fdea8
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-06-18 00:03:04 +0900

    Add new pages

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import '../public/style.scss'
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+export default () => {
+    return (
+        <>
+            <div className="background">
+                <div className="texts">
+                    <p className="texts__title">REIWA Graphics</p>
+                    <p className="texts__descriptions">あなたの仕事をさらに輝かせる<br />制作プロダクション</p>
+                </div>
+            </div>
+        </>
+    )
+};
