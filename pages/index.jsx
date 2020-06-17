48e9f5f5a5ec6e53f072d06624066b9be4f10c58
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-06-18 00:52:27 +0900

    Add movie

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,9 @@ export default () => {
     return (
         <>
             <div className="background">
+                <video id="ytplayer" autoplay="autoplay" loop muted playsinline src="./after.mp4"></video>\
+                <div class="overlay">
+                </div>
                 <div className="texts">
                     <p className="texts__title">REIWA Graphics</p>
                     <p className="texts__descriptions">あなたの仕事をさらに輝かせる<br />制作プロダクション</p>
