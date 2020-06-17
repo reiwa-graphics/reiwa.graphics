298e6b531d13fee64d864bcd36d8c3029f639337
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-06-18 01:02:03 +0900

    Update minify

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,7 @@ export default () => {
     return (
         <>
             <div className="background">
-                <video id="ytplayer" autoplay="autoplay" loop muted playsinline src="./after.mp4"></video>\
-                <div class="overlay">
-                </div>
+                <video id="ytplayer" autoplay="autoplay" loop muted playsInline src="./after.mp4" />
                 <div className="texts">
                     <p className="texts__title">REIWA Graphics</p>
                     <p className="texts__descriptions">あなたの仕事をさらに輝かせる<br />制作プロダクション</p>
