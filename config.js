a76398e3866475fc6b97c1d922e1c7b163e9e895
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-06-18 15:33:41 +0900

    Add meta

---
+++ config.js
@@ -0,0 +1,11 @@
+export const MY_SEO = {
+    title: 'MyTitle',
+    description: 'My description',
+    openGraph: {
+        type: 'website',
+        url: 'My URL',
+        title: 'MyTitle',
+        description: 'My description',
+        image: '...jpg',
+    }
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,18 @@
+import Meta from './meta';
+
 export default () => {
     return (
         <>
-            <div className="background">
-                <video id="ytplayer" autoPlay loop muted playsInline src="./after.mp4" />
-                <div className="texts">
-                    <p className="texts__title">REIWA Graphics</p>
-                    <p className="texts__descriptions">あなたの仕事をさらに輝かせる<br />制作プロダクション</p>
+            <Meta />
+            <main>
+                <div className="background">
+                    <video id="ytplayer" autoPlay loop muted playsInline src="./after.mp4" />
+                    <div className="texts">
+                        <p className="texts__title">REIWA Graphics</p>
+                        <p className="texts__descriptions">あなたの仕事をさらに輝かせる<br />制作プロダクション</p>
+                    </div>
                 </div>
-            </div>
+            </main>
         </>
     )
 };
